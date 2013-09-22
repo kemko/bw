764326ac8b3e6b1b8834ab7d3aac506433b5b1f2
Alexander Titov <titoff.a@gmail.com>
2013-09-22 12:14:49 +0400

    increase performance of app

--- roles/redmine.rb
+++ roles/redmine.rb
@@ -3,4 +3,8 @@ description "Install and configure redmine"
 run_list "recipe[runit]", "recipe[postgresql]", "recipe[ruby]", "recipe[redmine]"
 
 default_attributes(
+  'redmine' => {
+    'pgsql_conn' => 50,
+    'unucorn_proc' => 4
+  }
 )
